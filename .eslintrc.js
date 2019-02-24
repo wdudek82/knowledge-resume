31766941a54f0f2e63946f2008627116904e45c6
Wojciech Dudek <wdudek82@gmail.com>
2019-02-24 16:14:46 +0100

    Added eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'airbnb-base',
+  globals: {},
+  parserOptions: {
+    impliedStrict: true,
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ main.js
@@ -0,0 +1,1 @@
+const items = document.querySelectorAll('#template li');
