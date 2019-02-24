400beb47793683b4d039bd043b3af4dfc98e47fc
Wojciech Dudek <wdudek82@gmail.com>
2019-02-24 19:13:12 +0100

    Added boxes sliding animation

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-const items = document.querySelectorAll('#template li');
+const items = document.querySelectorAll('#timeline li');
+
+function isInViewport(e) {
+  const rect = e.getBoundingClientRect();
+  return (
+    rect.top >= 0
+    && rect.left >= 0
+    && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    && rect.right <= (window.innerWidth || decodeURIComponent.documentElement.clientWidth)
+  );
+}
+
+function run() {
+  items.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+    } else {
+      item.classList.remove('show');
+    }
+  });
+}
+
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
